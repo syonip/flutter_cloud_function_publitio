5049d3671e9b51b3ed827e9eb47924666983198a
Johnny <syonip@gmail.com>
2020-08-14 11:12:33 +0300

    first

---
+++ android/app/src/main/kotlin/com/learningsomethingnew/flutter_fbstorage_function/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.learningsomethingnew.flutter_fbstorage_function
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/apis/firebase_provider.dart
@@ -0,0 +1,35 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_cloud_function_publitio/models/video_info.dart';
+
+class FirebaseProvider {
+  static saveVideo(VideoInfo video) async {
+    await Firestore.instance.collection('videos').document().setData({
+      'videoUrl': video.videoUrl,
+      'thumbUrl': video.thumbUrl,
+      'coverUrl': video.coverUrl,
+      'aspectRatio': video.aspectRatio,
+      'uploadedAt': video.uploadedAt,
+      'videoName': video.videoName,
+    });
+  }
+
+  static listenToVideos(callback) async {
+    Firestore.instance.collection('videos').snapshots().listen((qs) {
+      final videos = mapQueryToVideoInfo(qs);
+      callback(videos);
+    });
+  }
+
+  static mapQueryToVideoInfo(QuerySnapshot qs) {
+    return qs.documents.map((DocumentSnapshot ds) {
+      return VideoInfo(
+        videoUrl: ds.data['videoUrl'],
+        thumbUrl: ds.data['thumbUrl'],
+        coverUrl: ds.data['coverUrl'],
+        aspectRatio: ds.data['aspectRatio'],
+        videoName: ds.data['videoName'],
+        uploadedAt: ds.data['uploadedAt'],
+      );
+    }).toList();
+  }
+}
