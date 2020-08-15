65473b66537ca22e7aeed751c9a1937c363807f2
Johnny <syonip@gmail.com>
2020-08-15 12:35:15 +0300

    delete function

--- cloud-functions/functions/index.js
+++ cloud-functions/functions/index.js
@@ -16,11 +16,40 @@ exports.uploadNewVideo = functions.firestore
 
         try {
             const data = await publitio.uploadFile(stream, 'file')
-            console.log('Uploading success. data:')
-            console.log(data)
+            console.log(`Uploading finished. status code: ${data.code}`)
+            if (data.code == 201) {
+                admin.firestore().collection("videos").doc(videoFile.name).set({
+                    finishedProcessing: true,
+                    videoUrl: data.url_download,
+                    thumbUrl: data.url_thumbnail,
+                    aspectRatio: data.width / data.height,
+                    publitioId: data.id,
+                }, { merge: true });
+            }
+
+            // Delete the source file if you want
+            bucket.file(context.params.videoId).delete()
         }
         catch (error) {
             console.error('Uploading error')
+            console.error(error)
+        }
+    });
+
+exports.deleteVideo = functions.firestore
+    .document('videos/{videoId}')
+    .onDelete(async (snap, context) => {
+        const videoId = context.params.videoId;
+        const publitioId = snap.data().publitioId;
+        console.log(`Deleting video file: ${videoId}`)
+
+        try {
+            const result = await publitio.call(`/files/delete/${publitioId}`, 'DELETE')
+            console.log('delete complete. result:')
+            console.log(result)
+        }
+        catch (error) {
+            console.error('Delete error')
             console.error(error)
         }
     });
--- cloud-functions/tests/index.js
+++ cloud-functions/tests/index.js
@@ -14,7 +14,7 @@ const PublitioAPI = require('publitio_js_sdk').default
 const publitioCredentials = require('../functions/publitio_credentials.json')
 const publitio = new PublitioAPI(publitioCredentials.key, publitioCredentials.secret)
 
-async function run() {
+async function runUpload() {
 
     const context = {
         params: {
@@ -22,32 +22,15 @@ async function run() {
         }
     }
 
-
     const bucket = admin.storage().bucket()
-
     const videoFile = bucket.file(context.params.videoId)
-
-    // const contentType = videoFile.contentType;
-    // const tempFilePath = path.join(os.tmpdir(), context.params.videoId);
-    // const metadata = {
-    //     contentType: contentType,
-    // };
-    // await videoFile.download({ destination: tempFilePath });
-    // console.log('Image downloaded locally to', tempFilePath);
-    // fs.unlinkSync(tempFilePath);
-    // return;
-
     const fileStream = videoFile.createReadStream();
     console.log(new Date().toString())
     console.log(`uploading video file: ${videoFile.name}`)
 
-    // const dataStream = new stream.PassThrough();
-    // fileStream.pipe(dataStream);
-
-
     try {
         const data = await publitio.uploadFile(fileStream, 'file')
-        
+
         console.log(new Date().toString())
         console.log('Uploading success. data:')
         console.log(data)
@@ -58,8 +41,9 @@ async function run() {
                 videoUrl: data.url_download,
                 thumbUrl: data.url_thumbnail,
                 aspectRatio: data.width / data.height,
-              }, { merge: true });
-            }
+                publitioId: data.id,
+            }, { merge: true });
+        }
     }
     catch (error) {
         console.log(new Date().toString())
@@ -68,4 +52,25 @@ async function run() {
     }
 }
 
-run();
+async function runDelete() {
+    const context = {
+        params: {
+            videoId: 'video1558'
+        }
+    }
+    const videoId = context.params.videoId;
+    const snap = await admin.firestore().collection("videos").doc(videoId).get();
+    const publitioId = snap.data().publitioId;
+    try {
+        const result = await publitio.call(`/files/delete/${publitioId}`, 'DELETE')
+        console.log('delete complete. result:')
+        console.log(result)
+    }
+    catch (error) {
+        console.error('Delete error')
+        console.error(error)
+    }
+}
+
+// runUpload();
+runDelete();
